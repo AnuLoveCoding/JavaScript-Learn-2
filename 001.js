e209769d10d4ddd99fbfca3017ce771c33666b62
anuLoveCoading <anusrivas07@gmail.com>
2022-09-11 21:52:04 +0530

    js syntax

--- 001.js
+++ 001.js
@@ -1,1 +1,59 @@
-console.log("ANURAG")
+// complication
+// code execution
+
+
+
+// why compilation
+
+
+
+
+
+// how javascript code executes
+
+
+
+
+// what is global context?
+// what is local execution?
+// closures
+
+// console.log(this);
+// console.log(window);
+// console.log(myfunction);
+// console.log(fullName);
+
+
+// function myfunction(){
+//     console.log("this is my function");
+// }
+
+
+
+// var firstName ="Anurag";
+// var lastName ="Mundra";
+// var fullName = firstName+" "+lastName;
+// console.log(fullName);
+
+// Uncaught RefrenceError:
+// Cannot acces "firstname" before Intialization
+console.log(firstName);
+let firstName ="Anurag";
+console.log(firstName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
