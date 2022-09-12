66dff8578920024c1b4de39342606e0072e3eced
anuLoveCoading <anusrivas07@gmail.com>
2022-09-12 23:05:57 +0530

    js syntax

--- 003.js
+++ 003.js
@@ -8,3 +8,14 @@
 
 // const ans = hello("arg");
 // ans();
+
+
+function myfuction(power){
+    return function(number){
+        return number**power;
+    }
+}
+
+const cube = myfuction(3);
+const ans =cube(6);
+console.log(ans);
